26f7eeb6672cadc81898ac9106fc073ae1d96c29
Billy Morris <Rivers@Billys-MacBook-Pro.local>
2017-01-03 20:27:30 -0800

    Completed setting up Firevabe

---
+++ SocialApp/SignInVC.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SocialApp
+//
+//  Created by Billy Morris on 1/3/17.
+//  Copyright © 2017 Billy Morris. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
