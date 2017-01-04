7622d54d1cccb2a5931d2d359cd3070779b3d81b
Billy Morris <Rivers@Billys-MacBook-Pro.local>
2017-01-04 00:06:29 -0800

    Facebook Authentication

--- SocialApp/SignInVC.swift
+++ SocialApp/SignInVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignInVC.swift
 //  SocialApp
 //
 //  Created by Billy Morris on 1/3/17.
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -21,6 +24,29 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("BILLY: Not able to authenticate with FB - \(error)")
+            } else if result?.isCancelled == true {
+                print("BILLY: User cancelled FB authentication")
+            } else {
+                print("BILLY: Successfully authenticated with FB")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("BILLY: Not able to authenticate with Firebase - \(error)")
+            } else {
+                print("BILLY: Successfully authenticated with Firebase")
+            }
+        })
+    }
 }
 
